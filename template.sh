3c4bb2b2b89bff9223495d74d518fc5fcfc3c951
Gadibavi ashok kumar reddy <ashoksku416@gmail.com>
2025-11-11 00:44:19 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,13 @@
+#Creating Folders
+mkdir -p src
+mkdir -p research
+#creating files
+touch src/main __init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+echo "Project structure created."
